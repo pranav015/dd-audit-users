a5548f2dd35ee23e5c1f9bacc9e7934a77e267a8
pranav015 <93210809+pranav015@users.noreply.github.com>
2022-06-29 11:04:38 -0700

    generate multiple reports (#3)
    * generate multiple reports
    
    * clean up code and reduce complexity

--- config/urls.rb
+++ config/urls.rb
@@ -8,8 +8,20 @@ def dd_eu_base_url
     'https://api.datadoghq.eu'
 end
 
+def dd_all_users_url
+    '/api/v2/users?page[size]=6000'
+end
+
 def dd_all_active_users_url
-    '/api/v2/users?page[size]=5000&filter[status]=Active'
+    '/api/v2/users?page[size]=6000&filter[status]=Active'
+end
+
+def dd_all_disabled_users_url
+    '/api/v2/users?page[size]=6000&filter[status]=Disabled'
+end
+
+def dd_all_pending_users_url
+    '/api/v2/users?page[size]=6000&filter[status]=Pending'
 end
 
 def dd_roles_base_url
--- datadog_user_list/datadog_audit.rb
+++ datadog_user_list/datadog_audit.rb
@@ -11,8 +11,8 @@ require 'faraday/net_http'
 require '../setup'
 require '../config/urls'
 
-def get_user_info
-    response = $conn.get($datadog_region + dd_all_active_users_url, nil, {  'Content-Type' => 'application/json' })
+def get_user_info(user_status_url)
+    response = $conn.get($datadog_region + user_status_url, nil, {  'Content-Type' => 'application/json' })
     @user_response = response.body["data"]
 end
 
@@ -55,10 +55,10 @@ def create_user_list
     end
 end
 
-def write_to_csv
+def write_to_csv(file_name)
     region =  $datadog_region == dd_us_base_url ? 'us' : 'eu'
     headers = ["Name", "Email", "Status", "Role Ids", "Role Names", "Created At", "Modified At"]
-    CSV.open("users_#{region}.csv", "w") do |csv|
+    CSV.open("#{file_name}_#{region}.csv", "w") do |csv|
         csv << headers
 
         @user_list.each do |user_row|
@@ -67,14 +67,33 @@ def write_to_csv
     end
 end
 
-def run_application
+def generate_audit_report(dd_url, file_name)
+    puts "Generating report for #{file_name}-users..."
+    
     # method calls (in sequential order)
     datadog_region_settings
     setup_connection
-    get_user_info
+    get_user_info(dd_url)
     get_role_info
     create_user_list
-    write_to_csv
+    write_to_csv(file_name)
+end
+
+def start_message
+    puts "Application running..."
+end
+
+def end_message
+    puts "Completed!"
+end
+
+def run_application
+    start_message
+    generate_audit_report(dd_all_users_url, "all")               # Generate report for all users
+    generate_audit_report(dd_all_active_users_url, "active")     # Generate report for all active users
+    generate_audit_report(dd_all_disabled_users_url, "disabled") # Generate report for all disabled users
+    generate_audit_report(dd_all_pending_users_url, "pending")   # Generate report for all pending users
+    end_message
 end
 
 run_application
